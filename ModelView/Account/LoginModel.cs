b098ea06d126e4c0af3b48e5b0f5353a5d5dd175
sanjiv kumar <ksanjiv0005@gmail.com>
2020-06-07 12:11:11 +0530

    datatype add

--- ModelView/Account/LoginModel.cs
+++ ModelView/Account/LoginModel.cs
@@ -9,8 +9,10 @@ namespace Exmination.Models
     public class LoginModel
     {
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string Username { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
